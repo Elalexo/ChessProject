865adea313633950569499467aea890f3e9b71e5
Elalexo <111273717+Elalexo@users.noreply.github.com>
2022-08-15 13:17:52 +0300

    Add files via upload

--- Project.cpp
+++ Project.cpp
@@ -1,50 +1,55 @@
 ﻿#include <iostream>
-#include <string>
 using namespace std;
-
-
- int main()
+int main()
 {
 	setlocale(LC_ALL, "ru");
-	int step1_1 ,step1_2 , step2_1,step2_2 ;
+	int step1_1, step1_2, step2_1, step2_2;
 	cin >> step1_1;
 	cin >> step1_2;
 
 	cin >> step2_1;
 	cin >> step2_2;
-	if (step1_1<0)
-	{
-		cout << "Выполнение невозможно";
-	}
-	if (step1_1 > 8)
-	{
-		cout << "Выполнение невозможно";
-	}
-	if (step1_2 < 0)
-	{
-		cout << "Выполнение невозможно";
-	}
-	if (step1_2 > 8)
-	{
-		cout << "Выполнение невозможно";
-	}
-	if (step2_1 < 0)
-	{
-		cout << "Выполнение невозможно";
-	}
-	if (step2_1 > 8)
-	{
-		cout << "Выполнение невозможно";
-	}
-	if (step2_2 < 0)
-	{
-		cout << "Выполнение невозможно";
-	}
-	if (step2_2 > 8)
-	{
-		cout << "Выполнение невозможно";
-	}
-
+		if (step1_1 <= 0)
+		{
+			cout << "Выполнение невозможно";
+			exit(0);
+		}
+		if (step1_1 > 8)
+		{
+			cout << "Выполнение невозможно";
+			exit(0);
+		}
+		if (step1_2 <= 0)
+		{
+			cout << "Выполнение невозможно";
+			exit(0);
+		}
+		if (step1_2 > 8)
+		{
+			cout << "Выполнение невозможно";
+			exit(0);
+		}
+		if (step2_1 <= 0)
+		{
+			cout << "Выполнение невозможно";
+			exit(0);
+		}
+		if (step2_1 > 8)
+		{
+			cout << "Выполнение невозможно";
+			exit(0);
+		}
+		if (step2_2 <= 0)
+		{
+			cout << "Выполнение невозможно";
+			exit(0);
+		}
+		if (step2_2 > 8)
+		{
+			cout << "Выполнение невозможно";
+			exit(0);
+		}
+		
 	if (step1_1<step2_1)
 	{
 		if (step1_2 < step2_2)
